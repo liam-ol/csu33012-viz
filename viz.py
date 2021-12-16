88437b095ea3bd3cc9f55ef0677f4b3a34aa734d
liam <lolionai@tcd.ie>
2021-12-16 23:42:42 +0000

    restructuring comments + TODO before bed...

--- viz.py
+++ viz.py
@@ -13,10 +13,9 @@ microsoft = git.get_organization("microsoft")
 vscode = git.get_repo("microsoft/vscode")
 
 # is_employee: Given a username string, return True if they are an employee of Microsoft.
-# Usually an employee is a member of Microsoft's GitHub 'organisation'.
-# Sometimes they are not, but have 'Microsoft' listed as their 'company' on their user page.
-# This function must check both.
 def is_employee(user):
+    # Usually an employee is a member of Microsoft's GitHub 'organisation'.
+    # Sometimes they are not, but have 'Microsoft' listed as their 'company' on their user page.
     if microsoft.has_in_members(git.get_user(user)) or "microsoft" in git.get_user(user).company.lower():
         return True
     else:
@@ -33,4 +32,5 @@ def process_user(contributor):
     # Convert this dict to a json object.
     return json.dumps(userdict)
 
-
+# TODO: FUNCTION TO CREATE JSON DATABASE
+# def write_database():
