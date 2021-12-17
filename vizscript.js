fae5c553d9ed3459cb91d22b57162b6efb709118
liam <lolionai@tcd.ie>
2021-12-17 20:55:18 +0000

    totalEmployeeContribs() function

--- vizscript.js
+++ vizscript.js
@@ -17,7 +17,17 @@ function totalEmployees(db) {
     return result;
 }
 
+// totalEmployeeContribs: Returns the sum of all Microsoft employee contributions.
+function totalEmployeeContribs(db) {
+    let result = 0;
+    db['database'].forEach(user =>
+        result += user['employed'] ? user['contribs'] : 0);
+    return result;
+}
+
 const employeeRatio = [totalEmployees(db), db['database'].length - totalEmployees(db)];
+const employeeContribRatio = [totalEmployeeContribs(db), totalContribs(db)];
 
 console.log("Number of contributions: " + totalContribs(db));
 console.log("Number of Microsoft employees: " + totalEmployees(db) + "/" + db['database'].length);
+console.log("Number of Microsoft employee contributions: " + totalEmployeeContribs(db));
