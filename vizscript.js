0b9f036afe291088bd3d8cb87cd5863b804f7547
liam <lolionai@tcd.ie>
2021-12-17 19:01:09 +0000

    Created vizscript.js totalContribs() function totalEmployees() function

---
+++ vizscript.js
@@ -0,0 +1,23 @@
+// Some extra functions to process the JSON database.
+d3.json("db.json").then(function(db) {
+
+    // totalContribs: Returns the sum of all contributions in the database.
+    function totalContribs(db) {
+        let result = 0;
+        db['database'].forEach(user =>
+            result += user['contribs']);
+        return result;
+    }
+
+    // totalEmployees: Returns the number of Microsoft employees in the database.
+    function totalEmployees(db) {
+        let result = 0;
+        db['database'].forEach(user =>
+            // Increment result if 'employed' is true.
+            result += user['employed'] ? 1 : 0);
+        return result;
+    }
+
+    console.log("Number of contributions: " + totalContribs(db));
+    console.log("Number of Microsoft employees: " + totalEmployees(db) + "/" + db['database'].length);
+})
